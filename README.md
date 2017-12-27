4942f7f966ed051dd99e7c68a345424bf6fe7afc
likai757 <likai757@gmail.com>
2017-12-28 07:23:10 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # webpackDemo
 Based on webpack / babel / react / less to build the basic framework
+
+```bash
+npm start   #launch dev server
+npm run dev #launch dev server 
+
+npm run build #build to public
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  devtool: 'cheap-module-source-map',
+  // devtool: 'cheap-module-source-map',
   entry: __dirname + '/app/index.js',//已多次提及的唯一入口文件
   output: {
     path: __dirname + '/public',//打包后的文件存放的地方
