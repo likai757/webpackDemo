aa06ec14914ce36c1e313b756826689ab882776f
likai757 <likai757@gmail.com>
2017-12-28 14:22:56 +0800

    增加mustache-loader，将.html 修改为.hbs 模板文件，并使用mustache-loader解析

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   // devtool: 'cheap-module-source-map',
   entry: __dirname + '/app/index.js',//已多次提及的唯一入口文件
   output: {
-    path: __dirname + '/public',//打包后的文件存放的地方
+    path: __dirname + '/build',//打包后的文件存放的地方
     filename: 'bundle.js'//打包后输出文件的文件名
   },
   devServer: {
@@ -16,37 +18,36 @@ module.exports = {
         test: /(\.jsx|\.js)$/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              'env', 'react'
-            ]
-          }
+          options: { presets: ['env', 'react'] }
         },
         exclude: /node_modules/
-      },
-      {
+      }, {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader',
-            options: {
-              // 指定启用css modules
-              modules: true,
-              // 指定css的类名格式
-              localIdentName: '[name]__[local]--[hash:base64:5]'
-            }
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                require('autoprefixer')
-              ]
-            }
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader', options: {
+            // 指定启用css modules
+            modules: true,
+            // 指定css的类名格式
+            localIdentName: '[name]__[local]--[hash:base64:5]'
           }
-        ]
+        }, {
+          loader: 'postcss-loader',
+          options: { plugins: [require('autoprefixer')] }
+        }]
+      }, {
+        test: /\.hbs?$/, use: [{ loader: 'mustache-loader' }]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有: Kai.Li'),
+    new htmlWebpackPlugin({
+      PATHS: [],
+      title: 'Webpack Sample Project',
+      template: __dirname + '/assets/index.hbs',
+      favicon: __dirname + '/assets/favicon.ico'
+    })
+  ]
 }
